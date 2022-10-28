b6e7022d601ee6c1f617f52e0ceba755bed6bd7d
Your Name~ <f.zihad333@gmail.com>
2022-10-28 21:16:18 +0600

    react 2 pdf added

--- src/components/CourseDetail/CourseDetail.js
+++ src/components/CourseDetail/CourseDetail.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { Link, useLoaderData } from "react-router-dom";
+import { DocumentArrowDownIcon } from "@heroicons/react/24/solid";
+
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const CourseDetail = () => {
   const {
@@ -18,16 +23,29 @@ const CourseDetail = () => {
   console.log(useLoaderData());
   return (
     <div className="container my-5 ">
-      <div className="name text-center my-5">
+      <div className="name text-center my-5 ">
         <h5>Couse Name </h5>
-        <h1>{name}</h1>
+        <h1>
+          {name}
+          <Pdf targetRef={ref} filename={`${name} course details.pdf`}>
+            {({ toPdf }) => (
+              <DocumentArrowDownIcon
+                onClick={toPdf}
+                style={{ width: "50px", cursor: "pointer", color: "tomato" }}
+              >
+                {" "}
+              </DocumentArrowDownIcon>
+            )}
+          </Pdf>
+        </h1>
       </div>
       <div className="detail-body d-flex flex-wrap flex-md-nowrap align-items-center gap-4">
         <div className="course-img my-5">
           <img src={img} alt="" className="img-fluid" />
         </div>
-        <div className="course-detail">
+        <div ref={ref} className="course-detail">
           <h4>details about the course</h4>
+          <p>{name}</p>
           <p>{shortDetail}</p>
           <hr />
           <p>Total Training Hour : {distribution.slice(0, 2)} Hour</p>
